9db883494517ed85aa1dc3fa08f34f8ed4b92336
Guilherme Proenca <guilherme130418@gmail.com>
2022-10-01 02:41:51 -0300

    Initial Commit

---
+++ src/MeuSegundoPrograma.java
@@ -0,0 +1,19 @@
+public class MeuSegundoPrograma {
+//Curso Java
+//Aula 04-01/10/22
+
+    public static void main(String[] args) {
+        System.out.println(2 + 2);
+        System.out.println(5 - 10);
+        System.out.println("Olá Mundo!");
+        System.out.println(15 / 3);
+
+        //Comentarios // -> sao uteis para indicar comentarios/ notas no projeto
+        //Ponto de Entrada - entry-point -> é o ponto onde todos os sistemas procuram no seu projeto  para
+        // comecar iniciar o projeto
+        // Metodo: sao instrucoes que define para o programa exectar. Exemplo:
+        // Intrucao chamada System.out.println("")
+        //Propriedades;Variaveis -> sera onde iremos armazenar dados para manipula-los no futuro
+
+        }
+}
